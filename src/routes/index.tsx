4f1357420a504df3ba28f14367dceb761936fb63
maiieul <maieul.chevalier@rosa.be>
2025-08-29 10:25:19 +0200

    chore: add qwik-ui components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,12 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, isBrowser, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Collapsible } from "@qwik-ui/headless";
+import { cn } from "@qwik-ui/utils";
+import { Button } from "~/components/ui";
+import { Alert } from "~/components/ui/alert/alert";
 
 export default component$(() => {
+  const switchSig = useSignal(false);
   return (
     <>
       <h1>Hi 👋</h1>
@@ -10,6 +15,26 @@ export default component$(() => {
         <br />
         Happy coding.
       </div>
+      <div>
+        <Alert.Root>
+          <Alert.Title>Alert</Alert.Title>
+          <Alert.Description>Alert Description</Alert.Description>
+        </Alert.Root>
+        <Button size="sm" onClick$={() => (switchSig.value = !switchSig.value)}>
+          Click Me
+        </Button>
+        {isBrowser && (
+          <div class={cn(switchSig.value ? "bg-red-500" : "bg-blue-500")}>
+            Browser
+          </div>
+        )}
+      </div>
+      <Collapsible.Root>
+        <Collapsible.Trigger>Open</Collapsible.Trigger>
+        <Collapsible.Content>
+          <p>Hello</p>
+        </Collapsible.Content>
+      </Collapsible.Root>
     </>
   );
 });
--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,20 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
  */
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths({ root: "." })],
+    build: {
+      minify: false,
+      rollupOptions: {
+        output: {
+          hoistTransitiveImports: false, // <— stops Rollup from injecting those bare imports
+          experimentalMinChunkSize: 0,
+        },
+      },
+    },
+    plugins: [
+      qwikCity(),
+      qwikVite({ debug: true }),
+      tsconfigPaths({ root: "." }),
+    ],
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
       // Put problematic deps that break bundling here, mostly those with binaries.
@@ -70,18 +83,18 @@ export default defineConfig(({ command, mode }): UserConfig => {
  */
 function errorOnDuplicatesPkgDeps(
   devDependencies: PkgDep,
-  dependencies: PkgDep,
+  dependencies: PkgDep
 ) {
   let msg = "";
   // Create an array 'duplicateDeps' by filtering devDependencies.
   // If a dependency also exists in dependencies, it is considered a duplicate.
   const duplicateDeps = Object.keys(devDependencies).filter(
-    (dep) => dependencies[dep],
+    (dep) => dependencies[dep]
   );
 
   // include any known qwik packages
   const qwikPkg = Object.keys(dependencies).filter((value) =>
-    /qwik/i.test(value),
+    /qwik/i.test(value)
   );
 
   // any errors for missing "qwik-city-plan"
